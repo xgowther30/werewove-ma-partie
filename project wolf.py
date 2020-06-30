5945797047f8ba00a02f23c33c2406842852c245
xgowther30 <65254810+xgowther30@users.noreply.github.com>
2020-06-30 20:41:49 +0200

    Add files via upload

---
+++ project wolf.py
@@ -0,0 +1,47 @@
+class werewolves(commands.cog):
+    def__init__(self,bot, logger, game):
+        self.bot = bot
+        self.logger = logger
+        self.game = game
+        bot.add_cog(werewolves(bot, logger, game))
+
+
+
+    @commands.group(ctx):
+    async def game(self, ctx):
+    
+    def game start(chanel):
+        chanel = werewolves-bot
+        liste = ls []
+        game object = role
+        role = mayor, villagers, psychic, witch, cupid, little girl, hunter, thief, saving, village idiot, scapegoat, the old, flute player, raven, the angel, the knight has the rusty sword, fox, devoted servant, bear watcher, stuttering judge, gypsy, the abominable sectarian, comedian, binoculars, the three brothers, werewolf, the big bad wolf, the infected father of wolves, white wolf, the wild child, wolf dog, cheer sellers, the oracle, the anarchist, the demon
+        mayor = none
+        ls.append game object in {liste}
+        @game start.check
+        def no_game start
+        return game start is none in {server}
+    if game start is not none:
+        await finish of game start
+        
+    else:
+        return game start()
+
+
+        class game():
+        def __init__(self, members)
+        self.members = members
+
+        def werewolve game(members):
+            members = has_role("werewolve game")
+            if ctx.message.content == 'ping' and members == true:
+
+
+        first_master = command author
+        print(first_master.self, firs_master.members)
+        ls.append first_master in {liste}
+        
+
+        
+
+
+    
