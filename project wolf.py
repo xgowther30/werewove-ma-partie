971997f2cf98952fe4987946c5126c77ee581e7b
xgowther30 <65254810+xgowther30@users.noreply.github.com>
2020-07-18 10:21:50 +0200

    Add files via upload

--- project wolf.py
+++ project wolf.py
@@ -1,3 +1,9 @@
+#!usr/bin/env python3
+#-*-coding:utf-8-*-
+from tkinter import *
+from timeit import default_timer
+
+
 class werewolves(commands.cog):
     def__init__(self,bot, logger, game):
         self.bot = bot
@@ -12,6 +18,7 @@ class werewolves(commands.cog):
     
     def game start(chanel):
         chanel = werewolves-bot
+        server = The Legend of Ttgc
         liste = ls []
         game object = role
         role = mayor, villagers, psychic, witch, cupid, little girl, hunter, thief, saving, village idiot, scapegoat, the old, flute player, raven, the angel, the knight has the rusty sword, fox, devoted servant, bear watcher, stuttering judge, gypsy, the abominable sectarian, comedian, binoculars, the three brothers, werewolf, the big bad wolf, the infected father of wolves, white wolf, the wild child, wolf dog, cheer sellers, the oracle, the anarchist, the demon
@@ -19,7 +26,7 @@ class werewolves(commands.cog):
         ls.append game object in {liste}
         @game start.check
         def no_game start
-        return game start is none in {server}
+        return game start is none in {chanel} of {server}
     if game start is not none:
         await finish of game start
         
@@ -39,7 +46,16 @@ class werewolves(commands.cog):
         first_master = command author
         print(first_master.self, firs_master.members)
         ls.append first_master in {liste}
-        
+
+        round = randint(0 , 10)
+        def updatetime():
+        time = default_timer()- start
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod (minutes, 60)
+        start = default_timer
+
+        if default_timer == 00:02:00:
+        round += 1
 
         
 
